11813a062762ccf00d4ac71645d2c325dbb76f0b
JOSE MAURICIO GALEANO CASTRILLON <mauricio.galeano2012@gmail.com>
2023-02-15 08:11:41 -0500

    The Rest API project was created for the iteration with the list of products through NodeJS and MongoDB.

--- controllers/Users.js
+++ controllers/Users.js
@@ -58,6 +58,7 @@ const updateUsers = (req, res) => {
 
 const deleteUsers = (req, res) => {
     Todo.deleteOne({ _id: req.params.userID })
-      .then(() => res.json({ message: "User Deleted" }))
+      .then(() => res.json({ message: "User Deletedd" }))
       .catch((err) => res.send(err));
+
   };
